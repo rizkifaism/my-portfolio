5d73a42aa87028e68cd86dca75193825c5cea01e
rizkifaism <rizkifais23@gmail.com>
2023-02-04 18:44:13 +0700

    menambahkan dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#btt");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("block");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("block");
   }
 };
 
@@ -14,7 +19,7 @@ window.onscroll = function () {
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener("click", function (e) {
+hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
@@ -25,3 +30,23 @@ document.addEventListener("click", function (e) {
     navMenu.classList.add("hidden");
   }
 });
+
+// Dark mode
+const darkMode = document.querySelector("#dark-mode");
+const html = document.querySelector("html");
+
+darkMode.addEventListener("click", function () {
+  if (darkMode.classList.toggle("dark-mode-active")) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkMode.classList.add("dark-mode-active");
+} else {
+  darkMode.classList.remove("dark-mode-active");
+}
